71fd1a4a13d54436fa3660a8a266f4366ef3f731
Juan Diego Trigos Duran <133736556+jtrigos30@users.noreply.github.com>
2025-11-28 03:24:56 -0500

    Change countdown target date and animation setup
    Updated target date for countdown timer and adjusted animation timings.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // --- CONFIGURACIÓN DEL TIEMPO ---
 // NOTA: Para probar ahora, dejamos esto así (Fecha Actual). 
 // Para el lanzamiento real, cambia a: const targetDate = new Date(2025, 10, 28, 15, 0, 0);
-const targetDate = new Date(2025, 10, 28, 16, 0, 0); 
+const targetDate = new Date(2025, 10, 28, 2, 0, 0); 
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -258,4 +258,5 @@ window.addEventListener('resize', () => {
 });
 
 // Iniciar animación
+
 animateParticles();
